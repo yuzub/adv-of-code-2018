82c1f5d33797fb70cad745d9b7bb188fde440d8c
Yuriy Zubaha <yuriy.zubaha@gmail.com>
2018-12-06 01:35:25 +0200

    day5 part2

--- day5/day5.js
+++ day5/day5.js
@@ -1,16 +1,22 @@
 const fs = require("fs");
 
 function polymer() {
-  fs.readFile("./day5-input-test.txt", (err, data) => {
+  fs.readFile("./day5-input.txt", (err, data) => {
+    console.time('timer');
     let inputPolymer = data.toString().trim();
     let polymer = inputPolymer;
 
     polymer = reactPolymer(polymer);
+    console.log('----------------------- PART 1 --------------------------------')
     // console.log(`Polymer after fully reacting is ${polymer}`);
     console.log(`Units remain ${polymer.length}`);
+    console.log('----------------------- PART 2 --------------------------------')
 
-    let arrPolymers = makeVarPolymer(inputPolymer);
-    console.log(arrPolymers);
+    let minPolCharAndLength = makeVarPolymer(inputPolymer);
+    console.log(`Removing all ${minPolCharAndLength.minCh}/${minPolCharAndLength.minCh.toLowerCase()} units is best.`);
+    console.log(`The length of the shortest polymer is ${minPolCharAndLength.minPolLength}.`);
+    console.log('-------------------------------------------------------')
+    console.timeEnd('timer');
   });
 }
 
@@ -53,24 +59,29 @@ function deleteTwoLetters(str, firstLetPosition) {
 }
 
 function makeVarPolymer(pol) {
-  console.log(pol);
-  let arrVarPolymer = [];
-  let ch = "";
-  let newPolymer = "";
-  let polLen = 0;
+  let ch = '', minCh = '';
+  let newPolymer = '';
+  let polLength = 0, minPolLength = pol.length;
   for (let code = 65; code < 90; code++) {
     ch = String.fromCharCode(code);
+    console.log(ch);
     if (pol.includes(ch)) {
-      newPolymer = pol.replace(ch, "").replace(ch.toLowerCase(), "");
-      polLen = newPolymer.length;
-      arrVarPolymer.push({
-        char: ch,
-        newPkl: newPolymer,
-        reactPolymerLength: polLen
-      });
+      // delete character - uppercase and lowercase
+      let regExp = new RegExp(`[${ch}${ch.toLowerCase()}]`, 'g');
+      newPolymer = pol.replace(regExp, '');
+      console.log(`All ${ch} deleted.`)
+      // react polymer
+      newPolymer = reactPolymer(newPolymer);
+      polLength = newPolymer.length;
+      console.log(`The length of the polymer is ${polLength}.`);
+
+      if (polLength < minPolLength) {
+        minCh = ch;
+        minPolLength = polLength;
+      }
     }
   }
-  return arrVarPolymer;
+  return { minCh: minCh, minPolLength: minPolLength };
 }
 
 polymer();

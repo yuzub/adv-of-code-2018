a1528d8ac28b20d4418c2452d101546721ea36d3
Yuriy Zubaha <yuriy.zubaha@gmail.com>
2018-12-10 01:37:03 +0200

    day9 part1 without log

--- day9/day9.js
+++ day9/day9.js
@@ -11,10 +11,10 @@ function marbleMania() {
     let highestScore = getHighestScore(players, lastMarble);
     console.log(`${players} players; last marble is worth ${lastMarble} points: the winning Elf's score = ${highestScore}`);
 
-    console.log('-----------------PART 2-----------------');
-    lastMarble *= 100;
-    highestScore = getHighestScore(players, lastMarble);
-    console.log(`${players} players; last marble is worth ${lastMarble} points: the winning Elf's score = ${highestScore}`);
+    // console.log('-----------------PART 2-----------------');
+    // lastMarble *= 100;
+    // highestScore = getHighestScore(players, lastMarble);
+    // console.log(`${players} players; last marble is worth ${lastMarble} points: the winning Elf's score = ${highestScore}`);
 
 
     console.timeEnd('timer');
@@ -29,7 +29,7 @@ function getHighestScore(pls, lMarb) {
   let playersScore = new Array(pls).fill(0);
 
   for (let marble = 2; marble <= lMarb; marble++) {
-    if (marble % 10000 === 0) console.log(marble);
+    // if (marble % 10000 === 0) console.log(marble);
     indCurPlayer = (indCurPlayer + 1 < pls) ? indCurPlayer + 1 : 0;
     if (marble % 23 !== 0) {
       indCurMarb = (indCurMarb + 2 <= circle.length) ? indCurMarb + 2 : 1;
